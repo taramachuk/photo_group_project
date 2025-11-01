de5054eb3b2621eea6a23e0bc0c041f777f4bd83
Taras Pryimachuk <taramachuk@gmail.com>
2025-11-01 13:44:59 +0100

    typo fix

--- src/main/java/com/example/backend/service/JwtService.java
+++ src/main/java/com/example/backend/service/JwtService.java
@@ -18,10 +18,10 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    @Value("${secuirty.jwt.secret-key")
+    @Value("${security.jwt.secret-key}")
     private String secretKey;
 
-    @Value("${security.jwt.expiration-time")
+    @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 
     public String extactUsername(String token) {
