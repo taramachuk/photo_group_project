67c96562765c0249cab603c7277e0d3a67f068c5
Patryk <patryk-romaniuk@o2.pl>
2025-11-19 18:38:06 +0100

    Create jwtservicetest

---
+++ src/test/java/com/example/backend/service/JwtServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.backend.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtServiceTest {
+
+    @InjectMocks
+    private JwtService jwtService;
+
+    private UserDetails userDetails;
+
+
+}
